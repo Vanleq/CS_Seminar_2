70b6c210ac80666a1405dd6e8af292d54b824eb5
Vadim <vanleq@mail.ru>
2024-02-11 13:29:03 +0300

    Task 1Е

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число");
+int N = Convert.ToInt32(Console.ReadLine());
+if (N % 7 == 0)
+{
+   if (N % 23 == 0);
+   Console.WriteLine("Число кратно и 7, и 23");
+}
+else
+{
+    Console.WriteLine("Число не кратно ни семи, ни двадцати трем");
+}
